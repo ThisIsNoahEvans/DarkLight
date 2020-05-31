af57218f01e1ca9c3e19961c3a603de2f5737944
Noah Evans <noah@itsnoahevans.co.uk>
2020-05-31 19:15:30 +0100

    Add v1.0

---
+++ DarkLight Menu Bar/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  DarkLight Menu Bar
+//
+//  Created by Noah Evans on 30/05/2020.
+//  Copyright © 2020 Noah Evans. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+            print("test")
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
